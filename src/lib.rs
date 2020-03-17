e607422fb1ff68b643a46ce3ca558492fc9b05f6
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-03-17 22:36:23 +0100

    Mount get_all_documents and unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate rocket_contrib;
 
+mod error;
+mod models;
 mod routes;
+mod schema;
+mod repository;
 
 #[cfg(test)]
 mod unit_tests;
 
+/// Create the rocket instance that represents our REST API
 pub fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/",
             routes![
             routes::hello_world,
+            routes::documents::get_all_documents,
             ])
+        .attach(repository::DbConn::fairing())
 }
--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -9,3 +9,37 @@ fn get_hello_world() {
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.body_string().unwrap(), "Hello, world!".to_string());
 }
+
+#[test]
+fn get_all_documents() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    mock_data(&rocket());
+
+    let response = client.get("/documents").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(
+        response
+        .content_type()
+        .expect("no content in response for /documents route"),
+        rocket::http::ContentType::JSON
+        );
+}
+
+#[cfg(test)]
+fn mock_data(rocket: &rocket::Rocket) {
+    use crate::schema::documents;
+    use crate::models::document::NewDocument;
+    use diesel::delete;
+    use diesel::RunQueryDsl;
+
+    let db_conn = super::repository::DbConn::get_one(&rocket).expect("could not connect to database");
+    delete(documents::table).execute(&db_conn.0).expect("could not clear documents table");
+    diesel::insert_into(documents::table)
+        .values(NewDocument {title: "First Document".to_string()})
+        .execute(&db_conn.0).expect("could not insert new document");
+    diesel::insert_into(documents::table)
+        .values(NewDocument {title: "Second Document".to_string()})
+        .execute(&db_conn.0).expect("could not insert new document");
+
+
+}
